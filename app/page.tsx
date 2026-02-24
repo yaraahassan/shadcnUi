828f4171ab11fabfb8c44df97614302d4952da56
YARA HASSAN <yarah5612@gmail.com>
2026-02-24 06:14:26 +0200

    feat:add shadcn project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+import { ThemeToggle } from "@/components/ui/theme-toggle";
+export default function Home() {
+  return (
+ <>
+ <Button variant="secondary">Button</Button>
+ <ThemeToggle/>
+
+ </>
+  );
+}
